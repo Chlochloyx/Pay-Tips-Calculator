455cb2e0efd86e5044d4c9f48b0d4d960673ba27
Ying Xiu <157107572+Chlochloyx@users.noreply.github.com>
2024-05-28 15:02:10 +0800

    Change tip percentage.py

--- tip calculator.py
+++ tip calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
 bill = int(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12 or 15?"))
+tip = int(input("How much tip would you like to give? 15, 18 or 20?"))
 people = int(input("How many people to split the bill?"))
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
